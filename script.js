dcacfdaa1b37655f37defbf94d5ec4e5757fe3d3
nsofa3966-a11y <nsofa3966@gmail.com>
2025-09-23 20:00:37 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const sheetUrls = [
-'https://docs.google.com/spreadsheets/d/e/2PACX-1vSUXau2tPe1749501W_g_CRNePsz9snloT7royNPt8L441VzhtCEHfCgcUZxa3bWBNDYb10ctatF6si/pub?output=tsv'
+'https://docs.google.com/spreadsheets/d/e/2PACX-1vTC1dGgA54hZEK2GKMnBdKLLy6IClu2kZohTAlxeQq6WR0lvAMTd0XOmOwDi4OQCFgh9GvEi2A-mzXN/pub?output=tsv'
   // 2-й слайд: лидер дня
 
 ];
